6ed8e43be7b06feea7c91a69f2fd8b5d84e911d5
collins777 <bmc1793@hotmail.com>
2025-06-29 11:14:59 -0400

    Refactor joke fetching logic to retrieve programming jokes and update display method

--- script.js
+++ script.js
@@ -7,15 +7,15 @@ const punchline = document.getElementById("punchline");
 jokeBtn.addEventListener("click", function () {
   console.log("Getting a joke...");
 
-  // Fetch a random joke from the Official Joke API
-  fetch("https://official-joke-api.appspot.com/random_joke")
+  // Fetch a random programming joke from the Official Joke API
+  fetch("https://official-joke-api.appspot.com/jokes/programming/random")
     .then(function (response) {
       // Convert the response to JSON format
       return response.json();
     })
     .then(function (joke) {
-      // Display the joke on the page
-      setup.textContent = joke.setup;
-      punchline.textContent = joke.punchline;
+      // Display the programming joke on the page
+      setup.textContent = joke[0].setup;
+      punchline.textContent = joke[0].punchline;
     });
 });
